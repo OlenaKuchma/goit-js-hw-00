c7920dda398809646e000ed2aa39052802b581da
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-15 16:10:16 +0300

    Update js-1.js

--- js/js-1.js
+++ js/js-1.js
@@ -1,9 +1,15 @@
- let base = prompt("Давай число");
- base = Number(base);
- console.log(base);
+//  let base = prompt("Давай число");
+//  base = Number(base);
+//  console.log(base);
 
- let power = prompt("Давай число");
- power = Number(power);
- console.log(power);
+//  let power = prompt("Давай степінь");
+//  power = Number(power);
+//  console.log(power);
 
+//  let result = base ** power;
+//  console.log(result);
  
+const max = 9;
+const min = 2;
+console.log(Math.random() * (max - min) + min);
+
