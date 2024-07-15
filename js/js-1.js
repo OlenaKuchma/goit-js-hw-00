4f31d845e86baaf9f2b7c5bf82ef0449ac40220f
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-15 15:58:36 +0300

    Update js-1.js

--- js/js-1.js
+++ js/js-1.js
@@ -1,2 +1,9 @@
- const base = 2;
- const power = 5;
+ let base = prompt("Давай число");
+ base = Number(base);
+ console.log(base);
+
+ let power = prompt("Давай число");
+ power = Number(power);
+ console.log(power);
+
+ 
