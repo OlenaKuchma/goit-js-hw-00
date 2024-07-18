ade5f8001898457bf7098db8d564fe4e524c48b1
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-18 16:32:27 +0300

    Update js-3.js

---
+++ js/js-3.js
@@ -0,0 +1,50 @@
+// const playlist = {
+//     name: 'My mysic',
+//     rating: 3,
+//     tracks: ['one', 'two', 'three'],
+//     trackCount: 3
+
+// }
+
+// const propertyName = 'tracks';
+
+// console.log(playlist);
+// console.log(playlist.name);
+// console.log(playlist.tracks);
+// console.log(playlist['tracks']);
+// console.log(playlist[propertyName]);
+
+// 1
+// const fn = function(myObjeckt) {
+//     console.log(myObjeckt);
+    
+// }
+
+// fn({a: 1, b: 2});
+
+// 2
+
+// const userName = 'Miki';
+// const userEmail = 'miki@gmail.com';
+
+// const userSignUp = {
+//     userName,
+//     userEmail,
+// }
+
+// console.log(userSignUp);
+
+// 3
+
+const inputName = 'Color';
+const inputValue = 'tomato';
+
+const colorPicerData = {
+    [inputName]: inputValue,
+    month: 4,
+}
+
+colorPicerData.day = 7;
+colorPicerData.month = 7;
+
+console.log(colorPicerData);
