f174317d8dd71340d59a9cd58ddd77f0fdd2f97f
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-22 18:40:43 +0300

    Update js-5.js

--- js/js-5.js
+++ js/js-5.js
@@ -19,53 +19,87 @@
 
 // console.log(doubleNumbers);
 
-// 2
- const players = [
-    { id: 'player-1', name: "Mango", timePlayed: 1270, points: 4, online: true },
-    { id: 'player-2', name: "Poly", timePlayed: 469, points: 2, online: false },
-    { id: 'player-3', name: "Ajax", timePlayed: 690, points: 3, online: true },
-    { id: 'player-4', name: "Kiwi", timePlayed: 241, points: 1, online: false },
-    { id: 'player-5', name: "Tony", timePlayed: 641, points: 7, online: false },
+// 2 -------------------------------map
+//  const players = [
+//     { id: 'player-1', name: "Mango", timePlayed: 1270, points: 4, online: true },
+//     { id: 'player-2', name: "Poly", timePlayed: 469, points: 2, online: false },
+//     { id: 'player-3', name: "Ajax", timePlayed: 690, points: 3, online: true },
+//     { id: 'player-4', name: "Kiwi", timePlayed: 241, points: 1, online: false },
+//     { id: 'player-5', name: "Tony", timePlayed: 641, points: 7, online: false },
 
- ]
+//  ]
 
- const playersName = players.map(player => player.name);
+//  const playersName = players.map(player => player.name);
 
- console.log(playersName);
-
-
- const playersId = players.map(player => player.id);
- console.log(playersId);
+//  console.log(playersName);
 
 
- const playersRes = players.map(({name, online}) => {
-    return {
-        name,
-        online
-    }
-});
- console.log(playersRes);
+//  const playersId = players.map(player => player.id);
+//  console.log(playersId);
 
 
- const updatePlayers = players.map(player => ({...player,
-        points: player.points*1.1, }));
+//  const playersRes = players.map(({name, online}) => {
+//     return {
+//         name,
+//         online
+//     }
+// });
+//  console.log(playersRes);
+
+
+//  const updatePlayers = players.map(player => ({...player,
+//         points: player.points*1.1, }));
  
-        console.table(updatePlayers);
+//         console.table(updatePlayers);
 
 
-const playerIdToUpdate = 'player-4';
+// const playerIdToUpdate = 'player-4';
  
-const updetedPlayer = players.map(player => {
+// const updetedPlayer = players.map(player => {
     
-    if (playerIdToUpdate === player.id) {
-        return {
-            ...player,
-            timePlayed: player.timePlayed + 100,
-        }
-    }
-        return player;
+//     if (playerIdToUpdate === player.id) {
+//         return {
+//             ...player,
+//             timePlayed: player.timePlayed + 100,
+//         }
+//     }
+//         return player;
 
-});
+// });
 
-    console.table(updetedPlayer);
+//     console.table(updetedPlayer);
 
+
+    // const updetedPlayer = players.map(player => 
+    //     playerIdToUpdate === player.id ? 
+    //     {...player, timePlayed: player.timePlayed + 100} : player);
+    
+    //     console.table(updetedPlayer);
+
+    // -------------------- filter
+
+    const numbers = [5, 10, 15, 20, 25];
+    const filteredNumbers = numbers.filter(number => {
+        console.log(number);
+
+        return number > 10;
+    });
+
+    console.log(filteredNumbers);
+
+
+    const players = [
+            { id: 'player-1', name: "Mango", timePlayed: 1270, points: 4, online: true },
+            { id: 'player-2', name: "Poly", timePlayed: 469, points: 2, online: false },
+            { id: 'player-3', name: "Ajax", timePlayed: 690, points: 3, online: true },
+            { id: 'player-4', name: "Kiwi", timePlayed: 241, points: 1, online: false },
+            { id: 'player-5', name: "Tony", timePlayed: 641, points: 7, online: false },
+        
+         ];
+    
+         const onlinePlayers = players.filter(player => player.online);
+         const offlinePlayers = players.filter(player => !player.online);
+
+ console.table(onlinePlayers);
+ console.table(offlinePlayers);
+ 
