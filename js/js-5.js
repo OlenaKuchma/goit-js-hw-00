bd14e3cbc2ddc60bf8279da8bb504ff216316257
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-23 15:26:11 +0300

    Update js-5.js

--- js/js-5.js
+++ js/js-5.js
@@ -139,20 +139,119 @@
 // -----------reduce
 
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
-const total = numbers.reduce((acc, number) => {
+// const total = numbers.reduce((acc, number) => {
     
-    console.log('number', number);
-    console.log('acc', acc);
+//     console.log('number', number);
+//     console.log('acc', acc);
     
     
-    return acc + number;
-}, 0);
+//     return acc + number;
+// }, 0);
 
-console.log(total);
+// console.log(total);
 
-const total1 = numbers.reduce((acc, number) => acc + number, 0);
+// const total1 = numbers.reduce((acc, number) => acc + number, 0);
 
-console.log(total1);
+// console.log(total1);
 
+
+// ------------------ reduce2
+
+// const peopleSalery = {
+//     Mango: 5000,
+//     Poly: 70000,
+//     Ajax: 800
+// };
+
+// console.table(peopleSalery);
+
+
+
+// const totalSalery = Object.values(peopleSalery).reduce((total, value) => total + value, 0);
+
+//     console.log(totalSalery);
+
+// --------------------reduce3
+
+
+    // const players = [
+    //         { id: 'player-1', name: "Mango", timePlayed: 1270, points: 4, online: true },
+    //         { id: 'player-2', name: "Poly", timePlayed: 469, points: 2, online: false },
+    //         { id: 'player-3', name: "Ajax", timePlayed: 690, points: 3, online: true },
+    //         { id: 'player-4', name: "Kiwi", timePlayed: 241, points: 1, online: false },
+    //         { id: 'player-5', name: "Tony", timePlayed: 641, points: 7, online: false },
+        
+    //      ];
+
+    //      const totalTimePlayed = players.reduce((totalTime, {timePlayed}) => totalTime + timePlayed, 0);
+
+    //      console.log('totalTimePlayed', totalTimePlayed);
+
+
+    // ------------------reduce4
+
+    // const twets = [
+    //     {id: '001', likes: 5, tags: ['js', 'nodes', 'react',  'css']},
+    //     {id: '002', likes: 15, tags: ['js',  'react', 'html', 'css']},
+    //     {id: '003', likes: 45, tags: ['js', 'nodes', 'html']},
+    //     {id: '004', likes: 25, tags: ['js', 'nodes', 'react', 'html']},
+    //     {id: '005', likes: 57, tags: ['nodes', 'react', 'css']}
+          
+    // ];
+
+    // const allTags = twets.reduce((tags, twet) => {
+           
+    //         tags.push(...twet.tags);
+    //         return tags;
+
+    //       }, []);
+
+
+    // // const allTags2 = twets.reduce((tags, twet) => [...tags, ...twet.tags],[]);
+
+    //       console.log(allTags);
+    //     //   console.log(allTags2);
+
+    // const tagsStats = allTags.reduce((acc, tag) => {
+       
+    //     console.log(acc);
+
+    //     if (acc[tag]) {
+
+    //         // acc[tag] += 1;   
+
+    //     return {
+    //         ...acc,
+    //         [tag]: acc[tag] + 1,
+    //     };
+    //     }
+
+    //     // acc[tag] = 1;
+
+    //     return {
+    //         ...acc,
+    //         [tag]: 1,
+    //     };
+    // }, {});
+    
+    // console.log(tagsStats);
+
+
+    // ---------sort
+
+
+    const numbers = [1, 3, 7, 2, 9, 11];
+
+    const copyNumbers = [...numbers];
+
+    copyNumbers.sort((curEl, nextEl) => nextEl - curEl);
+
+    console.log('copyNumbers', copyNumbers);
+
+    const laters = ['a', 'b', 'c', 'g', 's', 'f'];
+
+    laters.sort();
+
+    console.log('laters', laters);
