6a2e43a4351b95c40c9be43cae0f6414421a85f8
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-24 15:16:49 +0300

    Update js-6.js

--- js/js-6.js
+++ js/js-6.js
@@ -1,18 +1,198 @@
+// // різні комірки памяті
 
-// різні комірки памяті
+// console.log('[] === []', [] === []);
 
-console.log('[] === []', [] === []);
+// console.log('{} === {}', {} === {});
 
-console.log('{} === {}', {} === {});
+// console.log(' function() {} === function() {}', function() {} === function() {} );
 
-console.log(' function() {} === function() {}', function() {} === function() {} );
+// // посилання на той самий обєкт
 
+// const fnA = function () {};
 
+// const fnB = fnA;
 
-// посилання на той самий обєкт
+// console.log( 'fnA === fnB', fnA === fnB);
 
-const fnA = function () {};
+// ----------------------------------------
 
-const fnB = fnA;
+// const user = {
+// name: 'Mango',
+// showTag() {
+//     console.log('showTag -> this', this )
+// },
 
-console.log( 'fnA === fnB', fnA === fnB); 
+// };
+
+// user.showTag();
+
+// const foo = function () {
+
+//     console.log('foo -> this', this )
+// };
+
+// foo();
+
+// --------------------------!!!!!
+
+// const showTag = function () {
+
+//     console.log('showTag -> this', this );
+//     console.log('showTag -> this.tag', this.tag );
+
+// };
+
+// // showTag();
+
+// const user = {
+//     tag: 'Mango',
+
+//     };
+
+// user.showUserTag = showTag;
+
+// console.log('user', user);
+
+// user.showUserTag();
+
+// -------------------undefind.....
+
+// const user = {
+//     tag: 'Mango',
+//     showUserTag () {
+
+//         console.log('showTag -> this', this );
+//         console.log('showTag -> this.tag', this.tag );
+
+//     },
+
+//     };
+
+//     user.showUserTag();
+
+//     const outshowUserTag = user.showUserTag;
+
+//     outshowUserTag();
+
+// const invokeEction = function (action) {
+//     console.log(action);
+
+//     action();
+
+// };
+
+// invokeEction(user.showUserTag);
+
+// ---------------------------------
+
+// const makeChengeColor = function () {
+//     const changeColor = function (color) {
+//         console.log('changeColor -> this', this);
+
+//     };
+
+//     const sweater = {
+//         color:  'teal',
+//     };
+
+//     sweater.updateColor = changeColor;
+
+//     sweater.updateColor('red');
+
+//     return sweater.updateColor;
+// };
+
+// const swapColor = makeChengeColor();
+
+// swapColor('blue');
+
+// --------------------
+
+// const makeChengeColor = function () {
+//         const changeColor = function (color) {
+//             console.log('changeColor -> this', this);
+
+//         };
+//         return changeColor;
+//     };
+
+//     const updateColor = makeChengeColor();
+
+//     updateColor('yellow');
+
+//     const hat = {
+
+//         color: 'blue',
+//         updateColor,
+//     };
+
+//     console.log(hat.updateColor);
+
+//     hat.updateColor('orange');
+
+//     console.log(hat.color);
+
+// ----------
+
+// const counter = {
+//   value: 0,
+
+//   increment(value) {
+//     console.log("increment -> this", this), (this.value += value);
+//   },
+
+//   decrement(value) {
+//     console.log("decrement -> this", this), (this.value -= value);
+//   },
+// };
+
+// const updateCounter = function (value, operation) {
+//   operation(value);
+// };
+
+// updateCounter(10, counter.increment);
+// updateCounter(5, counter.decrement);
+
+//-----------------
+
+const showThis = function (a, b, c) {
+  console.log("showThis -> this", this);
+  console.log(a, b, c);
+};
+
+showThis();
+console.dir(showThis);
+
+const oBjA = {
+  a: 1,
+  b: 2,
+};
+
+showThis.call(oBjA, 2, 5, 7);
+showThis.apply(oBjA, [2, 5, 7]);
+
+const oBjB = {
+  a: 1111,
+  b: 24444,
+};
+
+showThis.call(oBjB, 2, 5, 7);
+
+const changeColor = function (color) {
+  console.log("changeColor -> this", this);
+  this.color = color;
+};
+
+const hat = {
+  color: "black",
+};
+
+changeColor.call(hat, "orange");
+console.log(hat);
+
+const sweater = {
+  color: "white",
+};
+
+changeColor.apply(sweater, ["olive"]);
+console.log(sweater);
