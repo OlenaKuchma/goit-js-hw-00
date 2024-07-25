5a57c869a51e9b1de30726f0fb3ff9fbef85e02b
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-07-25 19:18:01 +0300

    Update js-8.js

--- js/js-8.js
+++ js/js-8.js
@@ -9,20 +9,20 @@
 // console.log("allElementLi", allElementLi);
 // console.dir("allElementLi", allElementLi);
 
-const refs = {
-  seveBtn: document.querySelector(".save-btn"),
-  changeBtn: document.querySelector(".img-btn"),
-  inputEl: document.querySelector(".text-input"),
-  outputEl: document.querySelector(".text-output"),
-  firstEl: document.querySelector(".first-list"),
-  secondEl: document.querySelector(".second-list"),
-  imgEl: document.querySelector("img"),
-  imgTextEl: document.querySelector(".cat-text"),
-};
-console.log(refs);
+// const refs = {
+//   seveBtn: document.querySelector(".save-btn"),
+//   changeBtn: document.querySelector(".img-btn"),
+//   inputEl: document.querySelector(".text-input"),
+//   outputEl: document.querySelector(".text-output"),
+//   firstEl: document.querySelector(".first-list"),
+//   secondEl: document.querySelector(".second-list"),
+//   imgEl: document.querySelector("img"),
+//   imgTextEl: document.querySelector(".cat-text"),
+// };
+// console.log(refs);
 
-console.log(refs.imgEl.src);
-console.log(refs.imgEl);
+// console.log(refs.imgEl.src);
+// console.log(refs.imgEl);
 
 // // refs.imgEl.src =
 // //   "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSAOnLXSaPbc4K0IId0dSTI050OfwusYAyfQzMiCF6mrwNPVdmN";
@@ -62,9 +62,55 @@ console.log(refs.imgEl);
 // });
 // // console.log(firsNavEl.classList.contains("first-list"));
 
-const curentPageUrl = "./first";
+// const curentPageUrl = "./first";
 
-const linkEl = document.querySelector(`.first-a[href="${curentPageUrl}"]`);
-linkEl.classList.add("curent");
+// const linkEl = document.querySelector(`.first-a[href="${curentPageUrl}"]`);
+// linkEl.classList.add("curent");
 
-console.log(linkEl);
+// console.log(linkEl);
+
+// const nuvEl = document.querySelector(".list");
+
+// const firstElNuvEl = nuvEl.firstElementChild;
+
+// console.log(firstElNuvEl);
+
+// console.log(nuvEl.children[0]);
+
+// -----------------create
+
+const titelEl = document.createElement("h1");
+
+titelEl.classList.add("page-title");
+titelEl.textContent = "Заголовок сторінки";
+
+console.log(titelEl);
+
+console.log(document.body);
+
+document.body.appendChild(titelEl);
+
+const imgElement = document.createElement("img");
+imgElement.src =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Zoo_Wuppertal_Schwarzfusskatze.jpg/258px-Zoo_Wuppertal_Schwarzfusskatze.jpg";
+imgElement.alt = "foto";
+imgElement.width = 640;
+
+document.body.appendChild(imgElement);
+
+const newLiEl = document.createElement("li");
+newLiEl.classList.add = "first-list";
+
+const newALi = document.createElement("a");
+newALi.classList.add("first-a");
+newALi.textContent = "Нава сторінка";
+newALi.href = "/new";
+
+newLiEl.appendChild(newALi);
+
+console.log(newLiEl);
+
+const ulEl = document.querySelector(".list");
+ulEl.appendChild(newLiEl);
+
+ulEl.insertBefore(newLiEl, ulEl.firstChild);
